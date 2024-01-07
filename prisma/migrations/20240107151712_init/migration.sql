1c72150fbd99d3633112dcc0583febd1080a82ec
fahimahammed <fahimahammed.cse@gmail.com>
2024-01-07 21:23:46 +0600

    init

---
+++ prisma/migrations/20240107151712_init/migration.sql
@@ -0,0 +1,68 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `content` on the `Post` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `Post` table. All the data in the column will be lost.
+  - Made the column `bio` on table `Profile` required. This step will fail if there are existing NULL values in that column.
+  - Added the required column `age` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `country` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- CreateEnum
+CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
+
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "content",
+DROP COLUMN "updatedAt",
+ALTER COLUMN "title" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "Profile" ALTER COLUMN "bio" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "age" INTEGER NOT NULL,
+ADD COLUMN     "country" TEXT NOT NULL,
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "role" "Role" NOT NULL DEFAULT 'USER',
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "name" SET NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_CategoryToPost" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_CategoryToPost_AB_unique" ON "_CategoryToPost"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_CategoryToPost_B_index" ON "_CategoryToPost"("B");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPost" ADD CONSTRAINT "_CategoryToPost_A_fkey" FOREIGN KEY ("A") REFERENCES "Category"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_CategoryToPost" ADD CONSTRAINT "_CategoryToPost_B_fkey" FOREIGN KEY ("B") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ src/seed.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+    await prisma.user.deleteMany();
+    const userCreateInfo = await prisma.user.createMany({
+        data: [
+            {
+                age: 12,
+                name: "Søren Bramer-Schmidt",
+                email: "schmidt@prisma.io",
+                role: "ADMIN",
+                country: "Germany",
+            },
+            {
+                age: 7,
+                name: "Nikolas Burk",
+                email: "burk@prisma.io",
+                country: "Germany",
+            },
+        ],
+    });
+
+    console.log("Created users: ", userCreateInfo.count);
+}
+
+main()
+    .catch(err => console.error(err))
